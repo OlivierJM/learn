5d59bd2b83a7b6332e1bee5db838803c1a186ec4
Olivier JM <manolivier93@gmail.com>
2018-11-11 16:04:11 +0200

    added intro notes

---
+++ intro/intro.js
@@ -0,0 +1,9 @@
+/*
+
+ This is an introduction to Javascript and programming in general, we hope you find the 
+ notes here useful.
+ Notes in files will look like this one, you will find notes and examples explaining
+ different concepts
+ 
+ Read the intro.md to get started
+*/
